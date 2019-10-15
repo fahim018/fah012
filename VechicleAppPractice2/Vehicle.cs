f8bd363336b9df3acbf18cd4eafe2237dd615d5e
MdImranHabib <16103023@iubat.edu>
2019-10-15 19:25:03 +0600

    textFileAdded

---
+++ VechicleAppPractice2/Vehicle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2
+{
+   public class Vehicle
+   {
+       private string vehicleName;
+       private int registrationno;
+        public Vehicle(string vehiclename, int regno)
+        {
+            this.vehicleName= vehiclename;
+            this.registrationno = regno;
+        }
+        public string vehiclename { set; get; }
+        public int reno { set; get; }
+        public  List<Double> speed = new List<double>();
+
+        public double GetMax()
+        {
+            double max = speed.Max();
+            return max;
+        }
+        public double GetMin()
+        {
+            double min = speed.Min();
+            return min;
+        }
+        public double GetAvg()
+        {
+            double avg = speed.Average();
+            return avg;
+        }
+    }
+}
---
+++ VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    public partial class VehicleUi : Form
+    {
+
+        private Vehicle Vehicle;
+        public VehicleUi()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            Vehicle  = new Vehicle(vehicleNameTextBox.Text,Convert.ToInt32(regNoTextBox.Text));
+            vehicleNameTextBox.Clear();
+            regNoTextBox.Clear();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            Vehicle.speed.Add(Convert.ToDouble(speedTextBox.Text));
+            speedTextBox.Clear();
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            maxSpeedTextBox.Text = Vehicle.GetMax().ToString();
+            minSpeedTextBox.Text = Vehicle.GetMin().ToString();
+            averageSpeedTextBox.Text = Vehicle.GetAvg().ToString();
+        }
+    }
+}
